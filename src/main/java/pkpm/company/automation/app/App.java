ff34656c8fce79934ccf91900bf6040c393a69fe
Andrii Illiashenko <technolog2007@gmail.com>
2025-03-25 19:03:53 +0200

    second comit

--- src/main/java/pkpm/company/automation/app/App.java
+++ src/main/java/pkpm/company/automation/app/App.java
@@ -3,7 +3,7 @@ package pkpm.company.automation.app;
 import pkpm.company.automation.services.ExelReader;
 
 public class App {
-    private static final String FILE_NAME = "Графік.xlsx";
+    private static final String FILE_NAME = System.getenv("file_name");
     public static void main( String[] args ) {
 
         ExelReader.read(FILE_NAME);
